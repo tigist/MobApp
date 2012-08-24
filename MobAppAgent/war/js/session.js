fae7d50da0820930ea203d08d175ddda2a12dc09
tigist <tigist@almende.org>
2012-08-24 17:23:45 +0200

    updated phongap android application

--- MobAppAgent/war/js/session.js
+++
@@ -1,8 +0,0 @@
-
-session = {
-		  appServices:"http://memo-app-services.appspot.com/",
-	      sessionKey:"fooBar",
-	      isSession:function(){return true},
-	      addCallback:function(){},
-	      authenticator:function(){}
-};
