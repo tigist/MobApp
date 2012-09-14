9e0a8ad521e4d93cb1af46bd3cf4c1d4d1a14935
Ludo Stellingwerff <ludo@almende.org>
2012-09-14 10:43:55 +0200

    Fixed "bringAppToFront" behavior Released app to play store. (with all required assets) Made app work for multiple Android API versions.

--- MobApp2/src/com/mobapp2/MainActivity.java
+++ MobApp2/src/com/mobapp2/MainActivity.java
@@ -2,37 +2,35 @@ package com.mobapp2;
 
 import org.apache.cordova.DroidGap;
 
-import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends DroidGap{
-	 
-	private Button button;
+
 
 	public void onCreate(Bundle savedInstanceState) {
 		//final Context context = this;
-		
 		Window window = getWindow();
-	    window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-	    window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-	    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-	    setBooleanProperty("keepRunning", true);
+	    window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+	    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+		setBooleanProperty("keepRunning", true);
 	    super.setIntegerProperty("loadUrlTimeoutValue", 60000); 
 		super.onCreate(savedInstanceState);
 		super.clearCache();	
 		super.loadUrl("http://bridge-demo-mob-agent.appspot.com/"); 
 	 }
 	//Override back button to act like home button
-	
+	public void onStart(Bundle savedInstanceState){
+		Log.w("Bridge APP", "onStart called: opening screen");
+		Window window = getWindow();
+	    window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+	    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+	}
 	   @Override
 	   public boolean onKeyDown(int keyCode, KeyEvent event) {
 		   if (keyCode == KeyEvent.KEYCODE_BACK) {	   
@@ -43,7 +41,7 @@ public class MainActivity extends DroidGap{
 	   
 	   @Override
 	   public void onBackPressed(){
-		   Log.d("CDA", "onBackPressed Called");
+		   Log.d("Bridge APP", "onBackPressed Called");
 		   Intent intent = new Intent(Intent.ACTION_MAIN);
 	       intent.addCategory(Intent.CATEGORY_HOME);
 	       intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
