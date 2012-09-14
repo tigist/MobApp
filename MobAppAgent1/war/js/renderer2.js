191e5ebfde7d9a9a0353dbd042d17e324d400850
tigist <tigist@almende.org>
2012-09-14 11:06:22 +0200

    task list indenting modified

--- MobAppAgent1/war/js/renderer2.js
+++ MobAppAgent1/war/js/renderer2.js
@@ -4,23 +4,27 @@ var agentCache = null;
 var taskCache = null;
 var geocoder = null;
 
-$(document).ready(function (){
-	if (typeof(google) != "undefined") geocoder = new google.maps.Geocoder();
-	if (typeof(localStorage["Bridge_login"]) != "undefined" && localStorage["Bridge_login"] != null){
-		setupCacheAgent();
-		uuidValue=localStorage["Bridge_login"];
-		setupCacheTask();
+$(document).ready(
+		function() {
+			if (typeof (google) != "undefined")
+				geocoder = new google.maps.Geocoder();
+			if (typeof (localStorage["Bridge_login"]) != "undefined"
+					&& localStorage["Bridge_login"] != null) {
+				setupCacheAgent();
+				uuidValue = localStorage["Bridge_login"];
+				setupCacheTask();
 
-		if ($("#taskList1 .taskListItem").length == 0){
-			$("#tabs").tabs("select", 1);	
-		} else {
-			$("#tabs").tabs("select", 2);			
-		}
-		$("#loginTab").html("Logout");
-		document.getElementById('btnlogin').value = "logout";
-		document.getElementById('login').value=agentCache.getElement(uuidValue).login;
-	}
-});
+				if ($("#taskList1 .taskListItem").length == 0) {
+					$("#tabs").tabs("select", 1);
+				} else {
+					$("#tabs").tabs("select", 2);
+				}
+				$("#loginTab").html("Logout");
+				document.getElementById('btnlogin').value = "logout";
+				document.getElementById('login').value = agentCache
+						.getElement(uuidValue).login;
+			}
+		});
 document.addEventListener('deviceready', onDeviceReady, false);
 
 /**
@@ -34,17 +38,18 @@ function notification(source) {
 		window.plugins.PaigeSystemNotification.beep(1);
 		try {
 			window.plugins.webView.sendAppToFront();
-		} catch(e){
-			console.log("Couldn't get app to front:",e);
+		} catch (e) {
+			console.log("Couldn't get app to front:", e);
 		}
 	}
-	//Navigate to source page
-	if (source == "incoming"){
-		$("#tabs").tabs("select", 1);	
-	} else if (source == "operational"){
+	// Navigate to source page
+	if (source == "incoming") {
+		$("#tabs").tabs("select", 1);
+	} else if (source == "operational") {
 		$("#tabs").tabs("select", 2);
-	};
-	
+	}
+	;
+
 }
 
 /**
@@ -60,16 +65,17 @@ function onDeviceReady() {
 	}
 }
 /**
- * setupCacheAgent is a callback method to createcache for agents. 
- * It create a cache containes list of all agent detail. The cache renedere 
- * is created only once. The agent renderer callback used to retrieve current user 
- * from list of agents. 
+ * setupCacheAgent is a callback method to createcache for agents. It create a
+ * cache containes list of all agent detail. The cache renedere is created only
+ * once. The agent renderer callback used to retrieve current user from list of
+ * agents.
  */
 var setupCacheAgent = function() {
-	if (agentCache == null){
+	if (agentCache == null) {
 		if (!caches.exists("agent")) {
 			console.log("Init Cache agent");
-			agentCache = new ASKCache("agent", "agent/", null, "uuid", session, false); // cache for agents, Global!
+			agentCache = new ASKCache("agent", "agent/", null, "uuid", session,
+					false); // cache for agents, Global!
 			agentCache.addCB(plan_renderer);
 		} else {
 			agentCache = caches.getList("agent")[0];
@@ -79,10 +85,10 @@ var setupCacheAgent = function() {
 }
 
 /**
- * loginOkClciked method check the user input for the pin code. In the agent database 
- * we have agents with login code 1 - 5. If login code is different from the listed on
- * it display error message. If login correct the loading icon is visible. And it switch 
- * from the login tab to the incoming task tab
+ * loginOkClciked method check the user input for the pin code. In the agent
+ * database we have agents with login code 1 - 5. If login code is different
+ * from the listed on it display error message. If login correct the loading
+ * icon is visible. And it switch from the login tab to the incoming task tab
  */
 
 var loginClick = function() {
@@ -96,15 +102,14 @@ var loginClick = function() {
 			$("#message").html("Please enter your pin code");
 
 		} else {
-			checkAgent();//compare the login code with the existing agent cache
-			var loadingdiv2 = document.getElementById('loading0');
-			loadingdiv2.style.visibility = "visible";
+			checkAgent();// compare the login code with the existing agent
+							// cache
 			
 			var loadingdiv1 = document.getElementById('loading1');
 			loadingdiv1.style.visibility = "visible";
 
 			var loadingdiv2 = document.getElementById('loading2');
-			loadingdiv2.style.visibility = "visible";					
+			loadingdiv2.style.visibility = "visible";
 			var pin = document.getElementById('login').value;
 
 		}
@@ -127,23 +132,22 @@ checkAgent = function() {
 	if (document.getElementById('login').value != null) {
 		login = document.getElementById('login').value;
 		var agents = agentCache.getArray();
-		for (var i = 0; i < agents.length; i++) {
+		for ( var i = 0; i < agents.length; i++) {
 			if (agents[i].login == login) {
 				$("#loginTab").html("Logout");
 				document.getElementById('btnlogin').value = "logout";
 				uuidValue = agents[i].uuid;
-				localStorage["Bridge_login"]=uuidValue;
+				localStorage["Bridge_login"] = uuidValue;
 				setupCacheTask();
-				if ($("#taskList1 .taskListItem").length == 0){
-					$("#tabs").tabs("select", 1);	
+				if ($("#taskList1 .taskListItem").length == 0) {
+					$("#tabs").tabs("select", 1);
 				} else {
-					$("#tabs").tabs("select", 2);			
+					$("#tabs").tabs("select", 2);
 				}
 			}
 		}
 	}
 }
-
 
 /**
  * setupCacheTask is a callback method to create task cache for particular user.
@@ -160,19 +164,20 @@ var setupCacheTask = function() {
 
 	if (!caches.exists("task")) {
 		console.log("Application started loading task list..")
-		taskCache = new ASKCache("task", "agent/" + uuid + "/tasks", null, "id",session, false);// for task
+		taskCache = new ASKCache("task", "agent/" + uuid + "/tasks", null,
+				"id", session, false);// for task
 		taskCache.addRenderer('all', task_renderer);
 		taskCache.addCB(task_alert);
 		taskCache.render();
 	}
 }
 
-task_alert = function(oldVal,newVal){
-	if (oldVal == null && newVal != null){
-		if (newVal.data.state == "operational"){
+task_alert = function(oldVal, newVal) {
+	if (oldVal == null && newVal != null) {
+		if (newVal.data.state == "operational") {
 			notification("operational");
 		} else {
-			notification("incoming");			
+			notification("incoming");
 		}
 	}
 }
@@ -183,41 +188,48 @@ task_alert = function(oldVal,newVal){
  * and button through callback method newTAskcontainer.
  */
 task_renderer = function(json, olddata, cache) {
-	if (json && typeof(uuidValue) != "undefined") {
+	if (json && typeof (uuidValue) != "undefined") {
 		tasks = json;
 		newTaskContainer(tasks);
-		
+
 		/**
 		 * once task detail displayed on the screen hide the loading status
-		 */ 
+		 */
 		var loadingdiv1 = document.getElementById('loading1');
 		loadingdiv1.style.visibility = "hidden";
 		var loadingdiv2 = document.getElementById('loading2');
 		loadingdiv2.style.visibility = "hidden";
-		if($('#taskList .taskListItem').length== 0){
-			$('#taskList').html("<span class='noTasks'>No tasks to display!</span>");
+		if ($('#taskList .taskListItem').length == 0) {
+			$('#taskList').html(
+					"<span class='noTasks'>No tasks to display!</span>");
 		} else {
 			$('#taskList .noTasks').hide();
-		};
-		if($('#taskList1 .taskListItem').length== 0){
-			$('#taskList1').html("<span class='noTasks'>No tasks to display!</span>");
+		}
+		;
+		if ($('#taskList1 .taskListItem').length == 0) {
+			$('#taskList1').html(
+					"<span class='noTasks'>No tasks to display!</span>");
 		} else {
 			$('#taskList1 .noTasks').hide();
-		};
-		$( "#tabs" ).tabs();
+		}
+		;
+		$("#tabs").tabs();
 	}
 }
 
-plan_renderer = function(oldVal,newVal) {
-	if (newVal != null){
-		if (typeof(uuidValue) == "undefined" || newVal.id != uuidValue) return;
-		if (oldVal == null || oldVal.data.plan != newVal.data.plan){
+plan_renderer = function(oldVal, newVal) {
+	if (newVal != null) {
+		if (typeof (uuidValue) == "undefined" || newVal.id != uuidValue)
+			return;
+		if (oldVal == null || oldVal.data.plan != newVal.data.plan) {
 			console.log("plan changed");
-			setTimeout(function(){taskCache.render();},100);
+			setTimeout(function() {
+				taskCache.render();
+			}, 100);
 			notification("operational");
 		}
 	}
-		
+
 	var loadingdiv1 = document.getElementById('loading1');
 	loadingdiv1.style.visibility = "hidden";
 	var loadingdiv2 = document.getElementById('loading2');
@@ -233,70 +245,99 @@ plan_renderer = function(oldVal,newVal) {
  */
 newTaskContainer = function(tasks) {
 	$("#taskList, #taskList1").empty();
-	for (var i=0; i<tasks.length; i++){
+	for ( var i = 0; i < tasks.length; i++) {
 		taskState = tasks[i].state;
 		var resourceList = "<ul>";
 		var myState = "";
 		var agentStateOf = "";
-		var skipPlan=true;
+		var skipPlan = true;
 		var agentPlan = agentCache.getElement(uuidValue).plan;
 		var agentPlanLoc = null;
 		var toLocation = agentCache.getElement(uuidValue).toLocation;
-		if (toLocation != ""){
-			agentPlanLoc = {"lat":toLocation.split(" ")[0],"lon":toLocation.split(" ")[1]};	
+		if (toLocation != "") {
+			agentPlanLoc = {
+				"lat" : toLocation.split(" ")[0],
+				"lon" : toLocation.split(" ")[1]
+			};
 		}
 		for ( var j = 0; j < tasks[i].resources.length; j++) {
 			var type = tasks[i].resources[j].type;
 			if (type == "human") {
 				agentStateOf = tasks[i].resources[j].details.state;
-				if (tasks[i].state == "operational"){
-					if (agentStateOf == "accepted"){
-						resourceList += "<li>" + tasks[i].resources[j].details.name + '</li>';//loop in resources
+				if (tasks[i].state == "operational") {
+					if (agentStateOf == "accepted") {
+						resourceList += "<li>"
+								+ tasks[i].resources[j].details.name + '</li>';// loop
+																				// in
+																				// resources
 					}
 				} else {
-					if (tasks[i].resources[j].id == uuidValue){
-						myState=agentStateOf;
+					if (tasks[i].resources[j].id == uuidValue) {
+						myState = agentStateOf;
 					}
-					resourceList += "<li>" + tasks[i].resources[j].details.name + " ("+agentStateOf+")</li>";//loop in resources
-					
+					resourceList += "<li>" + tasks[i].resources[j].details.name
+							+ " (" + agentStateOf + ")</li>";// loop in
+																// resources
+
 				}
 			} else if (type == "car") {
 				resourceList += "<li>Vehicle: "
 						+ tasks[i].resources[j].details.name + '</li>';
 			}
 		}
-		resourceList +="</ul>";
-		if (tasks[i].state == "operational" && $("#taskList1 .taskListItem").length == 0){
-			skipPlan=false;
+		resourceList += "</ul>";
+		if (tasks[i].state == "operational"
+				&& $("#taskList1 .taskListItem").length == 0) {
+			skipPlan = false;
 		}
-		
-		if (geocoder != null){
-			var latLon = new google.maps.LatLng(tasks[i].lat,tasks[i].lon);
-			geocoder.geocode({location:latLon}, function(i){
-				return function(results,status){
-				if (status == google.maps.GeocoderStatus.OK) {
-					$('.loc_'+i).html(results[0].formatted_address);
-				}
-			}}(i));
-			if (agentPlanLoc != null){
-				latLon = new google.maps.LatLng(agentPlanLoc.lat,agentPlanLoc.lon);
-				geocoder.geocode({location:latLon}, function(i){
-					return function(results,status){
+
+		if (geocoder != null) {
+			var latLon = new google.maps.LatLng(tasks[i].lat, tasks[i].lon);
+			geocoder.geocode({
+				location : latLon
+			}, function(i) {
+				return function(results, status) {
 					if (status == google.maps.GeocoderStatus.OK) {
-						$('.planLoc_'+i).html(results[0].formatted_address);
+						$('.loc_' + i).html(results[0].formatted_address);
 					}
-				}}(i));				
+				}
+			}(i));
+			if (agentPlanLoc != null) {
+				latLon = new google.maps.LatLng(agentPlanLoc.lat,
+						agentPlanLoc.lon);
+				geocoder.geocode({
+					location : latLon
+				}, function(i) {
+					return function(results, status) {
+						if (status == google.maps.GeocoderStatus.OK) {
+							$('.planLoc_' + i).html(
+									results[0].formatted_address);
+						}
+					}
+				}(i));
 			}
 		}
-		var newTaskDetail = (skipPlan || agentPlan==""?"":"Plan: <b>"+agentPlan+"</b><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(<span class='planLoc_"+i+"'>("+agentPlanLoc+")</span>)<hr>")+ 
-				"Description:<br>" 
-				+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+tasks[i].description + '<br>'
-				+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(<span class='loc_"+i+"'>" + tasks[i].lat.substring(0,6) + ' , ' + tasks[i].lon.substring(0,5) + '</span>)<br>'
-				+ "Team: " + '<br>' + resourceList+"<br><hr>";
+		var newTaskDetail = (skipPlan || agentPlan == "" ? ""
+				: "Plan: <br> <b>" + agentPlan
+						+ "</b><br>(<span class='planLoc_" + i + "'>("
+						+ agentPlanLoc + ")</span>)<hr>")
+				+ "Description:<br>"
+				+ tasks[i].description
+				+ '<br>'
+				+ "(<span class='loc_"
+				+ i
+				+ "'>"
+				+ tasks[i].lat.substring(0, 6)
+				+ ' , '
+				+ tasks[i].lon.substring(0, 5)
+				+ '</span>)<br>'
+				+ "Team: "
+				+ '<br>' + resourceList + "<br><hr>";
 
 		/**
-		 * new accept button created and appended to the corresponding list html element
-		 */ 
+		 * new accept button created and appended to the corresponding list html
+		 * element
+		 */
 		var accept = document.createElement('input');
 		accept.type = "button";
 		accept.id = "btnAccept" + i;
@@ -304,8 +345,9 @@ newTaskContainer = function(tasks) {
 		accept.value = "Accept"
 		accept.title = "Accept";
 		accept.onclick = function(i) {
-			return function(){
-				postAccept(tasks[i].id); // accept i-th task and post acceptance to the server
+			return function() {
+				postAccept(tasks[i].id); // accept i-th task and post
+											// acceptance to the server
 				change(i); // disable button in i-th task
 			}
 		}(i);
@@ -314,8 +356,9 @@ newTaskContainer = function(tasks) {
 			accept.title = "you have already accepted this task";
 		}
 		/**
-		 * new reject button created and appended to the corresponding list html element
-		 */ 
+		 * new reject button created and appended to the corresponding list html
+		 * element
+		 */
 		var reject = document.createElement('input');
 		reject.type = "button";
 		reject.id = "btnreject" + i;
@@ -326,7 +369,8 @@ newTaskContainer = function(tasks) {
 		reject.disabled = true;
 
 		/**
-		 *  new started button created and appended to the corresponding html li element
+		 * new started button created and appended to the corresponding html li
+		 * element
 		 */
 
 		var start = document.createElement('input');
@@ -338,8 +382,9 @@ newTaskContainer = function(tasks) {
 		start.disabled = true;
 
 		/**
-		 * new withdraw button created and appended to the corresponding html li element
-		 */ 
+		 * new withdraw button created and appended to the corresponding html li
+		 * element
+		 */
 
 		var withdraw = document.createElement('input');
 		withdraw.type = "button";
@@ -350,26 +395,20 @@ newTaskContainer = function(tasks) {
 		withdraw.title = "withdraw";
 		withdraw.disabled = true;
 
-		
 		/**
-		 * attache the button to the current html list element in the same execution
-		 */ 
+		 * attache the button to the current html list element in the same
+		 * execution
+		 */
 		if (taskState == 'pending') {
-			$("<div class='taskListItem'>")
-				.html(newTaskDetail)
-				.append($(accept))
-				.append($(reject))
-			.appendTo($("#taskList"));
+			$("<div class='taskListItem'>").html(newTaskDetail).append(
+					$(accept)).append($(reject)).appendTo($("#taskList"));
 		} else {
-			$("<div class='taskListItem'>")
-				.html(newTaskDetail)	
-				.append($(start))
-				.append($(withdraw))
-			.appendTo($("#taskList1"));
+			$("<div class='taskListItem'>").html(newTaskDetail)
+					.append($(start)).append($(withdraw)).appendTo(
+							$("#taskList1"));
 		}
 	}
 }
-
 
 /**
  * change function used to detect the value of dynamically created button for
@@ -405,7 +444,7 @@ postAccept = function(taskId) {
 	var agentId = uuidValue;
 	var url = "http://memo-app-services.appspot.com/agent/" + agentId
 			+ "/tasks/" + taskId + "/accept";
-	$.ajax({
+$.ajax({
 		type : "POST",
 		xhrFields : {
 			withCredentials : true
@@ -413,7 +452,7 @@ postAccept = function(taskId) {
 		contentType : "application/json; charset=utf-8",
 		url : url,
 		success : function(msg) {
-			if (typeof(msg) == "string"){
+			if (typeof (msg) == "string") {
 				msg = JSON.parse(msg);
 			}
 			task_renderer(msg);
@@ -422,8 +461,8 @@ postAccept = function(taskId) {
 		error : function(a, b, c) {
 			console.log("HttpRequest: " + a + " textStatus: " + b
 					+ "errorThrown: " + c);
-			alert("HttpRequest: " + a + "<br>" + " textStatus:  " + b
-					+ "<br>" + "errorThrown: " + c);
+			alert("HttpRequest: " + a + "<br>" + " textStatus: " + b + "<br>"
+					+ "errorThrown: " + c);
 		}
 	});
 }
